bd3531432d2f23618cdb83894f9bf37d001847b5
“Davit <2840850M@student.gla.ac.uk>
2025-07-16 20:41:22 +0100

    Initial commit with Xcode project

---
+++ StudyDesk/StudyDesk/StudyDeskApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyDeskApp.swift
+//  StudyDesk
+//
+//  Created by Davit Muradyan on 16.07.25.
+//
+
+import SwiftUI
+
+@main
+struct StudyDeskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
